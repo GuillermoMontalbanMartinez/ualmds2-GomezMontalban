fbf62e1813fece2dcfa7947e4e190082185b52c5
GUILLERMO MONTALBAN MARTINEZ <guillermosysti@gmail.com>
2021-03-01 20:50:37 +0100

    vistas pedidos a entregar y productos a entregar (falta diseño)

--- local/GomezMontalban-mds2/frontend/src/vista-pedidos-a-entregar.js
+++ local/GomezMontalban-mds2/frontend/src/vista-pedidos-a-entregar.js
@@ -1,24 +1,40 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
+import './vista-producto-a-enviar.js';
 
-/**
- * `vista-pedidos-a-entregar`
- *
- * VistaPedidosAEntregar element.
- *
- * @customElement
- * @polymer
- */
 class VistaPedidosAEntregar extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-vertical-layout style="width: 100%; height: 100%;">
+ <vaadin-horizontal-layout theme="spacing" style="flex-shrink: 0; align-self: center;">
+  <vaadin-horizontal-layout theme="spacing">
+   <vaadin-button style="flex-shrink: 0; align-self: center;">
+    Productos a entregar
+   </vaadin-button>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing">
+   <vaadin-button>
+    Productos entregados
+   </vaadin-button>
+  </vaadin-horizontal-layout>
+ </vaadin-horizontal-layout>
+ <vista-producto-a-enviar style="height: 12%; align-self: center; padding: var(--lumo-space-xs); margin: var(--lumo-space-m); flex-shrink: 0;"></vista-producto-a-enviar>
+ <vista-producto-a-enviar style="height: 12%; align-self: center; flex-shrink: 0; padding: var(--lumo-space-xs); margin: var(--lumo-space-m);"></vista-producto-a-enviar>
+ <vista-producto-a-enviar style="align-self: center; height: 12%; flex-shrink: 0; margin: var(--lumo-space-m); padding: var(--lumo-space-xs);"></vista-producto-a-enviar>
+ <vista-producto-a-enviar style="height: 12%; align-self: center; flex-shrink: 0; margin: var(--lumo-space-m); padding: var(--lumo-space-xs);"></vista-producto-a-enviar>
+ <vista-producto-a-enviar style="height: 12%; margin: var(--lumo-space-m); padding: var(--lumo-space-xs); align-self: center; flex-shrink: 0;"></vista-producto-a-enviar>
+ <vista-producto-a-enviar style="height: 12%; margin: var(--lumo-space-m); padding: var(--lumo-space-xs); flex-shrink: 0; align-self: center;"></vista-producto-a-enviar>
+</vaadin-vertical-layout>
+`;
     }
 
     static get is() {
