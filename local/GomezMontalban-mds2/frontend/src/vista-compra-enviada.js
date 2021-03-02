22add599829ba8261a8e756e8abd388c5caa1479
David Gomez Lopez <dgl335@inlumine.ual.es>
2021-03-02 10:31:59 +0100

    Vista compra enviada

--- local/GomezMontalban-mds2/frontend/src/vista-compra-enviada.js
+++ local/GomezMontalban-mds2/frontend/src/vista-compra-enviada.js
@@ -1,24 +1,99 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
 
-/**
- * `vista-compra-enviada`
- *
- * VistaCompraEnviada element.
- *
- * @customElement
- * @polymer
- */
 class VistaCompraEnviada extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-vertical-layout style="width: 100%; height: 100%; align-items: center;">
+ <vaadin-horizontal-layout theme="spacing" style="margin-bottom: var(--lumo-space-m); margin-top: var(--lumo-space-m);">
+  <vaadin-horizontal-layout theme="spacing">
+   <label style="align-self: center;">Número del pedido</label>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing">
+   <vaadin-text-field></vaadin-text-field>
+  </vaadin-horizontal-layout>
+ </vaadin-horizontal-layout>
+ <vaadin-vertical-layout theme="spacing">
+  <vaadin-horizontal-layout theme="spacing" style="margin-bottom: var(--lumo-space-m);">
+   <vaadin-horizontal-layout theme="spacing">
+    <label style="align-self: center;">Nombre del cliente</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing">
+    <vaadin-text-field></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing">
+  <vaadin-horizontal-layout theme="spacing" style="margin-left: var(--lumo-space-xs); margin-bottom: var(--lumo-space-m);">
+   <vaadin-horizontal-layout theme="spacing">
+    <label style="align-self: center;">Dirección</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="padding-left: var(--lumo-space-xl); margin-left: var(--lumo-space-xl);">
+    <vaadin-text-field></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing">
+  <vaadin-horizontal-layout theme="spacing" style="margin-bottom: var(--lumo-space-m);">
+   <vaadin-horizontal-layout theme="spacing">
+    <label style="align-self: center;">Localidad</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="margin-left: var(--lumo-space-xl); padding-left: var(--lumo-space-xl);">
+    <vaadin-text-field></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing">
+  <vaadin-horizontal-layout theme="spacing" style="margin-bottom: var(--lumo-space-m);">
+   <vaadin-horizontal-layout theme="spacing">
+    <label style="align-self: center;">Calle</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="padding-left: var(--lumo-space-xl); margin-left: var(--lumo-space-xl);">
+    <vaadin-text-field style="margin-left: var(--lumo-space-xl); padding-left: var(--lumo-space-s);"></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing">
+  <vaadin-horizontal-layout theme="spacing" style="margin-bottom: var(--lumo-space-m);">
+   <vaadin-horizontal-layout theme="spacing">
+    <label style="align-self: center;">Portal</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="margin-left: var(--lumo-space-xl); padding-left: var(--lumo-space-xl);">
+    <vaadin-text-field style="padding-left: var(--lumo-space-l);"></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing">
+  <vaadin-horizontal-layout theme="spacing" style="margin-bottom: var(--lumo-space-m);">
+   <vaadin-horizontal-layout theme="spacing">
+    <label style="align-self: center;">Provincia</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="margin-left: var(--lumo-space-xl); padding-left: var(--lumo-space-xl);">
+    <vaadin-text-field></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing">
+  <vaadin-horizontal-layout theme="spacing">
+   <vaadin-horizontal-layout theme="spacing">
+    <label style="align-self: center;">Código postal</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="margin-left: var(--lumo-space-xl); padding-left: var(--lumo-space-xs);">
+    <vaadin-text-field></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+</vaadin-vertical-layout>
+`;
     }
     
 
