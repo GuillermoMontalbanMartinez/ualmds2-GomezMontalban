d6c3fed4b9c9f6c8ee32b5aeaea690d18f42e65c
David Gomez Lopez <dgl335@inlumine.ual.es>
2021-03-02 10:46:01 +0100

    Vista listado de compras enviadas y listado de compras

--- local/GomezMontalban-mds2/frontend/src/vista-listado-de-compras-enviadas.js
+++ local/GomezMontalban-mds2/frontend/src/vista-listado-de-compras-enviadas.js
@@ -1,24 +1,21 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
 
-/**
- * `vista-listado-de-compras-enviadas`
- *
- * VistaListadoDeComprasEnviadas element.
- *
- * @customElement
- * @polymer
- */
 class VistaListadoDeComprasEnviadas extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-vertical-layout style="width: 100%; height: 100%;">
+ <vaadin-text-field style="align-self: center; width: 100%; height: 100%;"></vaadin-text-field>
+</vaadin-vertical-layout>
+`;
     }
 
     static get is() {
