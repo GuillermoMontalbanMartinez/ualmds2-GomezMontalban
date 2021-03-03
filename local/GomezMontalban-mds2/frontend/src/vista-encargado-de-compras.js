a913d42f08a417c551ae4c0c459f93af47bdeb89
David Gomez Lopez <dgl335@inlumine.ual.es>
2021-03-03 23:14:09 +0100

    vista encargado de compras

--- local/GomezMontalban-mds2/frontend/src/vista-encargado-de-compras.js
+++ local/GomezMontalban-mds2/frontend/src/vista-encargado-de-compras.js
@@ -1,5 +1,7 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 class EncargadoDeCompras extends PolymerElement {
 
@@ -8,10 +10,24 @@ class EncargadoDeCompras extends PolymerElement {
 <style include="shared-styles">
                 :host {
                     display: block;
-                    height: 100%;
+                    height: 100%; 
                 }
             </style>
-<vaadin-vertical-layout style="width: 100%; height: 100%;"></vaadin-vertical-layout>
+<vaadin-vertical-layout style="width: 100%; height: 100%; align-items: center;">
+ <vaadin-horizontal-layout theme="spacing" style="margin-top: var(--lumo-space-xl); padding-top: var(--lumo-space-xl);">
+  <h1 style="width: 100%;">Bienvenido, seleccinone la lista que desea ver</h1>
+ </vaadin-horizontal-layout>
+ <vaadin-vertical-layout theme="spacing">
+  <vaadin-button theme="primary" style="width: 100%;">
+   Compras enviadas
+  </vaadin-button>
+ </vaadin-vertical-layout>
+ <vaadin-horizontal-layout theme="spacing" style="flex-shrink: 1;">
+  <vaadin-button theme="primary" style="align-self: center; width: 100%; margin-top: var(--lumo-space-m);">
+   Compras a enviar
+  </vaadin-button>
+ </vaadin-horizontal-layout>
+</vaadin-vertical-layout>
 `;
     }
 
