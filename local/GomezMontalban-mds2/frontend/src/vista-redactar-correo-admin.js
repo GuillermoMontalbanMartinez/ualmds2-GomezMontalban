4bc668f6f55625a08198388e109f57f497572045
GUILLERMO MONTALBAN MARTINEZ <guillermosysti@gmail.com>
2021-03-04 11:01:06 +0100

    Redactar correo (administrador)

--- local/GomezMontalban-mds2/frontend/src/vista-redactar-correo-admin.js
+++ local/GomezMontalban-mds2/frontend/src/vista-redactar-correo-admin.js
@@ -1,24 +1,63 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-area.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
-/**
- * `vista-contestar-email-admin`
- *
- * VistaContestarEmailAdmin element.
- *
- * @customElement
- * @polymer
- */
 class VistaContestarEmailAdmin extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-vertical-layout style="width: 100%; height: 80%;">
+ <vaadin-vertical-layout theme="spacing" style="align-self: center; align-items: center; justify-content: center; flex-shrink: 0; margin: var(--lumo-space-m);">
+  <h1 style="margin: var(--lumo-space-m); align-self: center;">Redactar correo</h1>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+  <vaadin-horizontal-layout theme="spacing">
+   <vaadin-horizontal-layout theme="spacing" style="margin: var(--lumo-space-m);">
+    <label style="margin: var(--lumo-space-m);">Destinatario</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="margin: var(--lumo-space-m);">
+    <vaadin-text-field style="flex-shrink: 0; align-self: center; margin: var(--lumo-space-m);"></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+  <vaadin-horizontal-layout theme="spacing">
+   <vaadin-horizontal-layout theme="spacing" style="margin: var(--lumo-space-m);">
+    <label style="flex-shrink: 0; align-self: center; margin-right: var(--lumo-space-s);">Asunto</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="margin: var(--lumo-space-m);">
+    <vaadin-text-field style="flex-shrink: 0; align-self: center; margin-left: var(--lumo-space-xl);"></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="align-self: center; margin: var(--lumo-space-m); align-items: center; justify-content: center; height: 100%; width: 60%;">
+  <vaadin-text-area style="width: 100%; height: 100%;"></vaadin-text-area>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="align-self: center; margin: var(--lumo-space-m);">
+  <vaadin-horizontal-layout theme="spacing" style="margin: var(--lumo-space-m);">
+   <vaadin-horizontal-layout theme="spacing" style="margin: var(--lumo-space-m);">
+    <vaadin-button style="flex-shrink: 0; align-self: center; margin: var(--lumo-space-m);">
+     Cancelar
+    </vaadin-button>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="margin: var(--lumo-space-m);">
+    <vaadin-button style="margin: var(--lumo-space-m); flex-shrink: 0; align-self: center;">
+     Aceptar
+    </vaadin-button>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+</vaadin-vertical-layout>
+`;
     }
 
     static get is() {
