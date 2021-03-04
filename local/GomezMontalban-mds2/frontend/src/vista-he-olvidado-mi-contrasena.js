1e1ec840615c001927198462975b779a30d529b1
GUILLERMO MONTALBAN MARTINEZ <guillermosysti@gmail.com>
2021-03-04 21:18:40 +0100

    borrar la contraseña y crear una nueva por un problema con el designer

---
+++ local/GomezMontalban-mds2/frontend/src/vista-he-olvidado-mi-contrasena.js
@@ -0,0 +1,54 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
+
+class VistaHeOlvidadoMiContrasena extends PolymerElement {
+
+    static get template() {
+        return html`
+<style include="shared-styles">
+                :host {
+                    display: block;
+                    height: 100%;
+                }
+            </style>
+<vaadin-vertical-layout style="width: 100%; margin: var(--lumo-space-m); align-items: center; justify-content: center;" theme="spacing">
+ <vaadin-vertical-layout theme="spacing" style="width: 100%;">
+  <h1 style="margin: var(--lumo-space-m); align-self: center; flex-shrink: 0;">¿Ha olvidado su contraseña?</h1>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+  <h4>Escriba su correo y se le enviará un formulario de recuperación de contraseña</h4>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="align-self: center; align-items: center; justify-content: center; flex-shrink: 0; margin: var(--lumo-space-m);">
+  <vaadin-horizontal-layout theme="spacing" style="margin: var(--lumo-space-m); flex-shrink: 0; align-self: center; align-items: center; justify-content: center;">
+   <vaadin-horizontal-layout theme="spacing" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+    <label style="margin: var(--lumo-space-m); flex-shrink: 0; align-self: center;">Correo electrónico</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing">
+    <vaadin-text-field></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+  <vaadin-button theme="primary">
+   Enviar
+  </vaadin-button>
+ </vaadin-vertical-layout>
+</vaadin-vertical-layout>
+`;
+    }
+
+    static get is() {
+        return 'vista-he-olvidado-mi-contrasena';
+    }
+
+    static get properties() {
+        return {
+            // Declare your properties here.
+        };
+    }
+}
+
+customElements.define(VistaHeOlvidadoMiContrasena.is, VistaHeOlvidadoMiContrasena);
---
+++ local/GomezMontalban-mds2/src/main/java/vistas/VistaHeOlvidadoMiContrasena.java
@@ -0,0 +1,31 @@
+package vistas;
+
+import com.vaadin.flow.templatemodel.TemplateModel;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+
+/**
+ * A Designer generated component for the vista-he-olvidado-mi-contrasena template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("vista-he-olvidado-mi-contrasena")
+@JsModule("./src/vista-he-olvidado-mi-contrasena.js")
+public class VistaHeOlvidadoMiContrasena extends PolymerTemplate<VistaHeOlvidadoMiContrasena.VistaHeOlvidadoMiContrasenaModel> {
+
+    /**
+     * Creates a new VistaHeOlvidadoMiContrasena.
+     */
+    public VistaHeOlvidadoMiContrasena() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+    /**
+     * This model binds properties between VistaHeOlvidadoMiContrasena and vista-he-olvidado-mi-contrasena
+     */
+    public interface VistaHeOlvidadoMiContrasenaModel extends TemplateModel {
+        // Add setters and getters for template properties here.
+    }
+}
