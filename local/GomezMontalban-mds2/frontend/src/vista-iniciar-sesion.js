bde68f7bee95da166381d8eb9b755833527df489
GUILLERMO MONTALBAN MARTINEZ <guillermosysti@gmail.com>
2021-03-04 21:26:54 +0100

    inicio de sesión del cibernauta

--- local/GomezMontalban-mds2/frontend/src/vista-iniciar-sesion.js
+++ local/GomezMontalban-mds2/frontend/src/vista-iniciar-sesion.js
@@ -1,24 +1,64 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-password-field.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
-/**
- * `vista-iniciar-sesion`
- *
- * VistaIniciarSesion element.
- *
- * @customElement
- * @polymer
- */
 class VistaIniciarSesion extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-vertical-layout style="width: 100%;">
+ <vaadin-vertical-layout theme="spacing" style="margin: var(--lumo-space-m); flex-shrink: 0; align-self: center; align-items: center; justify-content: center;">
+  <h1 style="margin: var(--lumo-space-m); flex-shrink: 0; align-self: center;">Inicio de sesión</h1>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center;">
+  <img style="width: 100%; height: 100%; margin: var(--lumo-space-m);">
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing-xs" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-xs);">
+  <vaadin-horizontal-layout theme="spacing-xs" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-xs);">
+   <vaadin-horizontal-layout theme="spacing" style="margin: var(--lumo-space-m); flex-shrink: 0; align-self: center; justify-content: center; align-items: center;">
+    <label style="margin: var(--lumo-space-m); flex-shrink: 0; align-self: center;">Correo</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center;">
+    <vaadin-text-field></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing-xs" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-xs);">
+  <vaadin-horizontal-layout theme="spacing" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+   <vaadin-horizontal-layout theme="spacing" style="margin: var(--lumo-space-m); flex-shrink: 0; align-self: center; align-items: center; justify-content: center;">
+    <label style="margin: var(--lumo-space-m); flex-shrink: 0; align-self: center;">Contraseña</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+    <vaadin-password-field label="Password" placeholder="Enter password" value="secret1"></vaadin-password-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="align-self: center; flex-shrink: 0; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+  <vaadin-button style="margin: var(--lumo-space-m); flex-shrink: 0; align-self: center;" theme="primary">
+   Inicio de sesión
+  </vaadin-button>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+  <vaadin-button style="align-self: center; flex-shrink: 0; margin: var(--lumo-space-m);" theme="primary">
+   He olvidado mi contraseña
+  </vaadin-button>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+  <vaadin-button style="flex-shrink: 0; align-self: center; margin: var(--lumo-space-m);" theme="primary">
+   Registrate
+  </vaadin-button>
+ </vaadin-vertical-layout>
+</vaadin-vertical-layout>
+`;
     }
 
     static get is() {
