93db7c65ab956cedfa2cee8615078eb938db1ad2
GUILLERMO MONTALBAN MARTINEZ <guillermosysti@gmail.com>
2021-03-04 10:41:59 +0100

    administración de cuentas de usuario (administrador)

--- local/GomezMontalban-mds2/frontend/src/vista-administrar-cuentas.js
+++ local/GomezMontalban-mds2/frontend/src/vista-administrar-cuentas.js
@@ -1,24 +1,33 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
-/**
- * `vista-administrar-cuentas`
- *
- * VistaAdministrarCuentas element.
- *
- * @customElement
- * @polymer
- */
 class VistaAdministrarCuentas extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-vertical-layout style="width: 100%;">
+ <vaadin-vertical-layout theme="spacing" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+  <h1 style="margin: var(--lumo-space-m);">Administrar cuentas de usuario</h1>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="align-self: center; flex-shrink: 0; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+  <vaadin-button theme="primary">
+   Dar de alta cuenta de usuario
+  </vaadin-button>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout theme="spacing" style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; margin: var(--lumo-space-m);">
+  <vaadin-button theme="primary">
+   Dar de baja cuenta de usuario
+  </vaadin-button>
+ </vaadin-vertical-layout>
+</vaadin-vertical-layout>
+`;
     }
 
     static get is() {
