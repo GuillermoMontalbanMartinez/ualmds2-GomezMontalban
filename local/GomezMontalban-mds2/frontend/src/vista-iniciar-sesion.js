114d79bb8b1c7ee50b0e31334793bd413c24f388
GUILLERMO MONTALBAN MARTINEZ <guillermosysti@gmail.com>
2021-03-05 14:37:30 +0100

    inicio de sesión modificación

--- local/GomezMontalban-mds2/frontend/src/vista-iniciar-sesion.js
+++ local/GomezMontalban-mds2/frontend/src/vista-iniciar-sesion.js
@@ -2,6 +2,7 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@vaadin/vaadin-login/src/vaadin-login-form.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-button/src/vaadin-button.js';
+import '@polymer/iron-icon/iron-icon.js';
 
 class VistaIniciarSesion extends PolymerElement {
 
@@ -13,17 +14,19 @@ class VistaIniciarSesion extends PolymerElement {
                     height: 100%;
                 }
             </style>
-<vaadin-vertical-layout style="width: 100%;" id="vista-inicio-de-sesion">
+<vaadin-vertical-layout style="width: 100%;background-color: #74EBD5;
+background-image: linear-gradient(90deg, #74EBD5 0%, #9FACE6 100%);
+" id="vista-inicio-de-sesion">
  <vaadin-vertical-layout theme="spacing" style="margin: var(--lumo-space-m); flex-shrink: 0; align-self: center; align-items: center; justify-content: center;">
   <h1 style="margin: var(--lumo-space-m); flex-shrink: 0; padding: var(--lumo-space-m);">Inicio de sesión</h1>
  </vaadin-vertical-layout>
  <vaadin-vertical-layout style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center; width: 100%; margin: var(--lumo-space-m);" id="vaadinVerticalLayout-de-la-imagen-inicio-de-sesion">
-  <img style="width: 100%; height: 100%; margin: var(--lumo-space-m); flex-shrink: 0; align-self: center;" id="img-login">
+  <iron-icon style="flex-shrink: 0; align-self: center; width: 10%; height: 100%;" icon="lumo:user"></iron-icon>
  </vaadin-vertical-layout>
  <vaadin-login-form style="flex-shrink: 0; align-self: center;" id="vaadinLoginForm-login-inicio-de-sesion"></vaadin-login-form>
  <vaadin-vertical-layout style="flex-shrink: 0; align-self: center; align-items: center; justify-content: center;" id="vaadinVerticalLayout-registrarse-inicio-de-sesion">
   <vaadin-button style="flex-shrink: 0; align-self: center; margin: var(--lumo-space-m);" theme="primary" id="boton-registrarse-inicio-de-sesion">
-    Registrate 
+   Registrarse
   </vaadin-button>
  </vaadin-vertical-layout>
 </vaadin-vertical-layout>
