f67f5c14c74a6340289b207d268750bf962a6f51
David Gomez Lopez <dgl335@inlumine.ual.es>
2021-03-12 14:35:55 +0100

    commit

--- local/GomezMontalban-mds2/src/main/java/com/example/test/MainView.java
+++ local/GomezMontalban-mds2/src/main/java/com/example/test/MainView.java
@@ -9,6 +9,9 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+
+import interfaz.Cibernauta;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -30,8 +33,11 @@ import org.springframework.beans.factory.annotation.Autowired;
         enableInstallPrompt = false)
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-public class MainView extends VerticalLayout {
 
+public class MainView extends VerticalLayout {
+	
+	Cibernauta cibernauta = new Cibernauta();
+	
     /**
      * Construct a new Vaadin view.
      * <p>
@@ -40,27 +46,41 @@ public class MainView extends VerticalLayout {
      * @param service The message service. Automatically injected Spring managed bean.
      */
     public MainView(@Autowired GreetService service) {
-
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-        textField.addThemeName("bordered");
-
-        // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello",
-                e -> Notification.show(service.greet(textField.getValue())));
-
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button has a more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
-
-        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-        addClassName("centered-content");
-
-        add(textField, button);
+//
+//        // Use TextField for standard text input
+//        TextField textField = new TextField("Your name");
+//        textField.addThemeName("bordered");
+//
+//        // Button click listeners can be defined as lambda expressions
+//        Button button = new Button("Say hello",
+//                e -> Notification.show(service.greet(textField.getValue())));
+//
+//        // Theme variants give you predefined extra styles for components.
+//        // Example: Primary button has a more prominent look.
+//        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+//
+//        // You can specify keyboard shortcuts for buttons.
+//        // Example: Pressing enter in this view clicks the Button.
+//        button.addClickShortcut(Key.ENTER);
+//
+//        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
+//        addClassName("centered-content");
+//
+//        add(textField, button);
+    	this.add(cibernauta);
+    	add(cibernauta);
+    	login();
+    	
     }
+
+	public void login() {
+		cibernauta._banner_no_registrado._iniciar_sesion.getVaadinLoginFormLoginInicioDeSesion().addLoginListener(e -> {
+			cibernauta.layout.remove(cibernauta._banner_no_registrado._iniciar_sesion);
+			Cibernauta.layour.add(Cibernauta.this._banner_no_registrado._iniciar_sesion);
+			
+			if (e)
+		});
+		
+	}
 
 }
