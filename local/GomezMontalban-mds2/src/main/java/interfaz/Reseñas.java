c51786effb18cdc9580a0edbc3806ac98ebcb362
David Gomez Lopez <dgl335@inlumine.ual.es>
2021-03-16 12:10:43 +0100

    merge

--- local/GomezMontalban-mds2/src/main/java/interfaz/Reseñas.java
+++ local/GomezMontalban-mds2/src/main/java/interfaz/Reseñas.java
@@ -2,6 +2,6 @@ package interfaz;
 
 public class Reseñas {
 	public Ver_carácteristicas_del_producto _ver_carácteristicas_del_producto;
-	public Carácteristicas_del_producto_admin _caracteristicas_del_producto_admin;
+	public Caracteristicas_del_producto_admin _caracteristicas_del_producto_admin;
 	public Reseña _reseña;
 }
