a43430a22ce258abda5147dc33428a0a43828d0f
David Gomez Lopez <dgl335@inlumine.ual.es>
2021-03-16 12:41:29 +0100

    Encargado de compras

--- local/GomezMontalban-mds2/frontend/src/vista-encargado-de-compras.js
+++ local/GomezMontalban-mds2/frontend/src/vista-encargado-de-compras.js
@@ -13,18 +13,18 @@ class VistaEncargadoDeCompras extends PolymerElement {
                     height: 100%;
                 }
             </style>
-<vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%; align-items: center;">
+<vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%; align-items: center;" id="layout_encargado_de_compras">
  <vaadin-horizontal-layout theme="spacing">
   <h1>Bienvenido, selecciona la lista que desea ver</h1>
  </vaadin-horizontal-layout>
  <vaadin-vertical-layout theme="spacing">
-  <vaadin-button theme="primary">
-   Compras enviadas
+  <vaadin-button theme="primary" id="button_compras_enviadas">
+    Compras enviadas 
   </vaadin-button>
  </vaadin-vertical-layout>
  <vaadin-horizontal-layout theme="spacing">
-  <vaadin-button theme="primary">
-   Compras a enviar
+  <vaadin-button theme="primary" id="button_compras_a_enviar">
+    Compras a enviar 
   </vaadin-button>
  </vaadin-horizontal-layout>
 </vaadin-vertical-layout>
--- local/GomezMontalban-mds2/src/main/java/interfaz/Encargado_de_compras.java
+++ local/GomezMontalban-mds2/src/main/java/interfaz/Encargado_de_compras.java
@@ -1,9 +1,47 @@
 package interfaz;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
 import vistas.VistaEncargadoDeCompras;
 
 
 public class Encargado_de_compras extends VistaEncargadoDeCompras {
 	public Pedidos_entregados _pedidos_entregados;
 	public Pedidos_a_entregar _pedidos_a_entregar;
+	public VerticalLayout layout;
+	
+	
+	public Encargado_de_compras() {
+		_pedidos_entregados = new Pedidos_entregados();
+		_pedidos_a_entregar = new Pedidos_a_entregar();
+		layout = this.getLayout_encargado_de_compras().as(VerticalLayout.class);
+		
+		this.getButton_compras_enviadas().addClickListener(new ComponentEventListener() {
+			
+			@Override
+			public void onComponentEvent(ComponentEvent event) {
+				
+				layout.removeAll();
+				layout.add(_pedidos_entregados);
+				
+			}
+		});
+		
+		
+		
+		this.getButton_compras_a_enviar().addClickListener(new ComponentEventListener() {
+			
+			@Override
+			public void onComponentEvent(ComponentEvent event) {
+				
+				layout.removeAll();
+				layout.add(_pedidos_a_entregar);
+				
+			}
+		});
+	}
 }
