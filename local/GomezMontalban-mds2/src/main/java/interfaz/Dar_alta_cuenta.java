f77572b52c16f99faf46f744ac538e58ad2dae8e
David Gomez Lopez <dgl335@inlumine.ual.es>
2021-05-03 18:05:53 +0200

    alta cuenta

--- local/GomezMontalban-mds2/src/main/java/interfaz/Dar_alta_cuenta.java
+++ local/GomezMontalban-mds2/src/main/java/interfaz/Dar_alta_cuenta.java
@@ -6,6 +6,7 @@ import com.vaadin.event.MouseEvents.ClickEvent;
 import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
@@ -27,11 +28,15 @@ public class Dar_alta_cuenta extends VistaDarAltaCuenta {
 	public void Alta_cuenta() throws PersistentException { 
 		ad = new BDPrincipal();	
 		String nombre = this.getTextNombre().getValue();
+		System.out.println(nombre);
 		String contrasena =this.getTextContrasena().getValue();
 		String correo =this.getTextCorreo().getValue();
-		
-		
-		ad.Alta_cuenta(nombre, "gomez", contrasena, 0, correo);
+		this.getTextNombre().setValue("FFFFF");
+		Notification.show(contrasena);
+		Notification.show(nombre);
+		Notification.show(correo);
+
+		//ad.Alta_cuenta(nombre, "gomez", contrasena, 0, correo);
 
 	}
 }
