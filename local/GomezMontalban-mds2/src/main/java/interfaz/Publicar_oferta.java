1f2136fe9462d92f7e8cbf6e664e5438701cc7b6
GUILLERMO MONTALBAN MARTINEZ <guillermosysti@gmail.com>
2021-05-05 11:27:18 +0200

    cambios

--- local/GomezMontalban-mds2/src/main/java/interfaz/Publicar_oferta.java
+++ local/GomezMontalban-mds2/src/main/java/interfaz/Publicar_oferta.java
@@ -9,6 +9,7 @@ import com.vaadin.flow.component.select.Select;
 import basededatos.BDPrincipal;
 import basededatos.Categoria;
 import basededatos.Oferta;
+import basededatos.Producto;
 import vistas.VistaPublicarOferta;
 
 public class Publicar_oferta extends VistaPublicarOferta {
@@ -19,35 +20,39 @@ public class Publicar_oferta extends VistaPublicarOferta {
 	Select<Oferta> select = new Select<>();
 	
 	public Publicar_oferta() {
-//		this.getButtonPublicarOferta().addClickListener(new ComponentEventListener() {
-//			@Override
-//			public void onComponentEvent(ComponentEvent event) {
-//				try {
-//					Alta_oferta();
-//				} catch (PersistentException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//		
-		//select.setItemLabelGenerator(Oferta::getId_ofertas()); -- Tiene que ser de tipo String no hay forma
+
+		this.getButtonPublicarOferta().addClickListener(new ComponentEventListener() {
+			@Override
+			public void onComponentEvent(ComponentEvent event) {
+				try {
+					Alta_oferta_producto(0, descuento, fechaLimite);
+				} catch (PersistentException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		
-		
-//		select.setItemLabelGenerator(Categoria::getNombre);
-//		try {
-//			select.setItems(this.cargar_categoria());
-//		} catch (PersistentException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		this.getLayoutSelectBajasCategorias().add(select);
+
+		select.setItemLabelGenerator(Producto::getNombre);
+		try {
+			select.setItems(this.cargar_categoria());
+		} catch (PersistentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.getLayoutSelectBajasCategorias().add(select);
 		
 	}
 
-	public void Alta_oferta() {
+	public void Alta_oferta_producto(int aId_producto, double aDescuento, String aFechaLimite) {
 		bd = new BDPrincipal();
 		descuento = Double.parseDouble(this.getTextDescuentoOferta().getValue().toString());
 		fechaLimite = this.getTextFechaLimiteOfertaProducto().getValue().toString();
 		bd.Alta_oferta_producto(0, descuento, fechaLimite);
 	}
+	
+	public void Alta_oferta_categoria(int aId_categoria, double aDescuento, String aFechaLimite) {
+		throw new UnsupportedOperationException();
+	}
+
 }
