91c1896243504bd59a0426dd3e70a1053984e590
GUILLERMO MONTALBAN MARTINEZ <guillermosysti@gmail.com>
2021-05-16 12:36:59 +0200

    vista producto arreglado

--- local/GomezMontalban-mds2/frontend/src/vista-producto.js
+++ local/GomezMontalban-mds2/frontend/src/vista-producto.js
@@ -1,5 +1,10 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
+import '@polymer/iron-icon/iron-icon.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-area.js';
 
 class VistaProducto extends PolymerElement {
 
@@ -12,7 +17,45 @@ class VistaProducto extends PolymerElement {
         			width: 100%;
                 }
             </style>
-<vaadin-vertical-layout style="width: 100%; border: #ccc solid 2px; border-radius : 20px; background: radial-gradient(circle, rgba(174,238,236,1) 63%, rgba(239,248,249,1) 100%);"></vaadin-vertical-layout>
+<vaadin-vertical-layout style="width: 100%; border: #ccc solid 2px; border-radius : 20px; background: radial-gradient(circle, rgba(174,238,236,1) 63%, rgba(239,248,249,1) 100%);">
+ <vaadin-vertical-layout style="align-self: center;">
+  <vaadin-horizontal-layout>
+   <vaadin-horizontal-layout>
+    <label style="margin: var(--lumo-space-m);">Producto</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout>
+    <vaadin-text-field id="textNombre" style="margin: var(--lumo-space-m);" readonly></vaadin-text-field>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout style="width: 100%; margin: var(--lumo-space-m);">
+  <vaadin-horizontal-layout style="width: 100%;">
+   <vaadin-horizontal-layout style="width: 30%;">
+    <img id="idFoto" style="width: 100%; height: 100%; margin: var(--lumo-space-m);">
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout style="width: 70%;">
+    <vaadin-text-area id="textDescripcion" style="width: 100%;" readonly></vaadin-text-area>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-vertical-layout style="margin: var(--lumo-space-m); align-self: center;">
+  <vaadin-horizontal-layout style="align-self: center;">
+   <vaadin-horizontal-layout>
+    <vaadin-horizontal-layout>
+     <label style="align-self: center; flex-shrink: 0; margin: var(--lumo-space-m);">Precio: </label>
+    </vaadin-horizontal-layout>
+    <vaadin-horizontal-layout>
+     <vaadin-text-field id="textPrecio" style="align-self: center; margin: var(--lumo-space-m);" readonly></vaadin-text-field>
+    </vaadin-horizontal-layout>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout>
+    <vaadin-button theme="icon" aria-label="Add new" id="buttonAgregarCarrito" style="align-self: center;">
+     <iron-icon icon="lumo:plus"></iron-icon>
+    </vaadin-button>
+   </vaadin-horizontal-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+</vaadin-vertical-layout>
 `;
     }
 
--- local/GomezMontalban-mds2/src/main/java/vistas/VistaProducto.java
+++ local/GomezMontalban-mds2/src/main/java/vistas/VistaProducto.java
@@ -6,6 +6,9 @@ import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 
 /**
  * A Designer generated component for the vista-producto template.
@@ -17,8 +20,17 @@ import com.vaadin.flow.component.polymertemplate.Id;
 @JsModule("./src/vista-producto.js")
 public class VistaProducto extends PolymerTemplate<VistaProducto.VistaProductoModel> {
 
-    @Id("agregarAlCarrito")
-	private Button agregarAlCarrito;
+
+	@Id("idFoto")
+	private Image idFoto;
+	@Id("textDescripcion")
+	private TextArea textDescripcion;
+	@Id("buttonAgregarCarrito")
+	private Button buttonAgregarCarrito;
+	@Id("textPrecio")
+	private TextField textPrecio;
+	@Id("textNombre")
+	private TextField textNombre;
 
 	/**
      * Creates a new VistaProducto.
@@ -34,11 +46,45 @@ public class VistaProducto extends PolymerTemplate<VistaProducto.VistaProductoMo
         // Add setters and getters for template properties here.
     }
 
-	public Button getAgregarAlCarrito() {
-		return agregarAlCarrito;
+	public Image getIdFoto() {
+		return idFoto;
 	}
 
-	public void setAgregarAlCarrito(Button agregarAlCarrito) {
-		this.agregarAlCarrito = agregarAlCarrito;
+	public void setIdFoto(Image idFoto) {
+		this.idFoto = idFoto;
 	}
+
+	public TextArea getTextDescripcion() {
+		return textDescripcion;
+	}
+
+	public void setTextDescripcion(TextArea textDescripcion) {
+		this.textDescripcion = textDescripcion;
+	}
+
+	public Button getButtonAgregarCarrito() {
+		return buttonAgregarCarrito;
+	}
+
+	public void setButtonAgregarCarrito(Button buttonAgregarCarrito) {
+		this.buttonAgregarCarrito = buttonAgregarCarrito;
+	}
+
+	public TextField getTextPrecio() {
+		return textPrecio;
+	}
+
+	public void setTextPrecio(TextField textPrecio) {
+		this.textPrecio = textPrecio;
+	}
+
+	public TextField getTextNombre() {
+		return textNombre;
+	}
+
+	public void setTextNombre(TextField textNombre) {
+		this.textNombre = textNombre;
+	}
+
+
 }
